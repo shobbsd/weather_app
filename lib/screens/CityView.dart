db2d76f3c5f1a79a56bc419148a6db6572d66fb7
Shaq <shobbsdaley@gmail.com>
2020-05-04 13:12:50 +0100

    initial commit

---
+++ lib/screens/CityView.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CityView extends StatefulWidget {
+  @override
+  _CityViewState createState() => _CityViewState();
+}
+
+class _CityViewState extends State<CityView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Text("cityView"),
+      ),
+    );
+  }
+}
