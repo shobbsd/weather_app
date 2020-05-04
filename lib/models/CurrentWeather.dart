b2785917e654eded0fc4d58c24eb27db9fb4374f
Shaq <shobbsdaley@gmail.com>
2020-05-04 18:01:58 +0100

    finalising currentWeather model

--- lib/models/CurrentWeather.dart
+++ lib/models/CurrentWeather.dart
@@ -1,44 +1,48 @@
+import 'package:intl/intl.dart';
+
 class CurrentWeather {
-  final Map<String, dynamic> coord;
-  final List<dynamic> weather;
-  final String base;
-  final Map<String, dynamic> main;
+  final String cityName;
+  final String icon;
+  final int temp;
+  final int minTemp;
+  final int windSpeed;
+  final int pressure;
+  final int humidity;
   final int visibility;
-  final Map<String, dynamic> wind;
-  final Map<String, dynamic> clouds;
-  final int dt;
-  final Map<String, dynamic> sys;
-  final int id;
-  final String name;
-  final int cod;
+  final String sunrise;
+  final String sunset;
 
   CurrentWeather(
-      {this.base,
-      this.clouds,
-      this.cod,
-      this.coord,
-      this.dt,
-      this.id,
-      this.main,
-      this.name,
-      this.sys,
+      {this.cityName,
+      this.icon,
+      this.temp,
+      this.minTemp,
+      this.windSpeed,
+      this.pressure,
+      this.humidity,
       this.visibility,
-      this.weather,
-      this.wind});
+      this.sunrise,
+      this.sunset});
 
   factory CurrentWeather.fromJson(Map<String, dynamic> json) {
+    var sunriseTimestamp = json['sys']['sunrise'];
+    var sunsetTimestamp = json['sys']['sunset'];
+    var sunrise = DateFormat.jm()
+        .format(DateTime.fromMillisecondsSinceEpoch(sunriseTimestamp * 1000));
+    var sunset = DateFormat.jm()
+        .format(DateTime.fromMillisecondsSinceEpoch(sunsetTimestamp * 1000));
+
     return CurrentWeather(
-        base: json['base'],
-        clouds: json['clouds'],
-        cod: json['cod'],
-        coord: json['coord'],
-        dt: json['dt'],
-        id: json['id'],
-        main: json['main'],
-        name: json['name'],
-        sys: json['sys'],
-        visibility: json['visibility'],
-        weather: json['weather'],
-        wind: json['wind']);
+      cityName: json['name'],
+      icon: json['weather'][0]['icon'],
+      temp: json['main']['temp'].round(),
+      minTemp: json['main']['temp_min'].round(),
+      windSpeed: json['wind']['speed'].round(),
+      pressure: json['main']['pressure'].round(),
+      humidity: json['main']['humidity'].round(),
+      visibility: (json['visibility'] / 1000).round(),
+      sunrise: sunrise,
+      sunset: sunset,
+    );
   }
 }
