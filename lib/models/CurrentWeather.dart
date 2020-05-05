f03cd7b07a6f8d8bdaa018bf5d83434cd52102b7
Shaq <shobbsdaley@gmail.com>
2020-05-05 21:48:28 +0100

    current weather changes
    added property updated

--- lib/models/CurrentWeather.dart
+++ lib/models/CurrentWeather.dart
@@ -11,9 +11,14 @@ class CurrentWeather {
   final int visibility;
   final String sunrise;
   final String sunset;
+  final String updated;
+  final int maxTemp;
+  final String description;
 
   CurrentWeather(
       {this.cityName,
+      this.description,
+      this.maxTemp,
       this.icon,
       this.temp,
       this.minTemp,
@@ -22,27 +27,34 @@ class CurrentWeather {
       this.humidity,
       this.visibility,
       this.sunrise,
+      this.updated,
       this.sunset});
 
   factory CurrentWeather.fromJson(Map<String, dynamic> json) {
-    var sunriseTimestamp = json['sys']['sunrise'];
-    var sunsetTimestamp = json['sys']['sunset'];
+    int sunriseTimestamp = json['sys']['sunrise'];
+    int sunsetTimestamp = json['sys']['sunset'];
+    int updatedTimestamp = json['dt'];
     var sunrise = DateFormat.jm()
         .format(DateTime.fromMillisecondsSinceEpoch(sunriseTimestamp * 1000));
     var sunset = DateFormat.jm()
         .format(DateTime.fromMillisecondsSinceEpoch(sunsetTimestamp * 1000));
 
+    var updated = DateFormat.Hm()
+        .format(DateTime.fromMillisecondsSinceEpoch(updatedTimestamp * 1000));
+
     return CurrentWeather(
-      cityName: json['name'],
-      icon: json['weather'][0]['icon'],
-      temp: json['main']['temp'].round(),
-      minTemp: json['main']['temp_min'].round(),
-      windSpeed: json['wind']['speed'].round(),
-      pressure: json['main']['pressure'].round(),
-      humidity: json['main']['humidity'].round(),
-      visibility: (json['visibility'] / 1000).round(),
-      sunrise: sunrise,
-      sunset: sunset,
-    );
+        cityName: json['name'],
+        icon: json['weather'][0]['icon'],
+        description: json['weather'][0]['description'],
+        temp: json['main']['temp'].round(),
+        minTemp: json['main']['temp_min'].round(),
+        maxTemp: json['main']['temp_max'].round(),
+        windSpeed: json['wind']['speed'].round(),
+        pressure: json['main']['pressure'].round(),
+        humidity: json['main']['humidity'].round(),
+        visibility: (json['visibility'] / 1000).round(),
+        sunrise: sunrise,
+        sunset: sunset,
+        updated: updated);
   }
 }
